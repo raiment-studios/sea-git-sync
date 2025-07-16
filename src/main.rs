5bef1f88f8b7111abd922c10a12c74bd28091ea2
Ridley Winters <ridley.grenwood.winters@gmail.com>
2025-07-16 14:39:29 -0700

    Sync changes

--- src/main.rs
+++ src/main.rs
@@ -64,7 +64,7 @@ fn sync_to_remote(args: &Args) -> Result<()> {
 
     git(&["add", "."])?;
     git(&["commit", "-m", &args.message])?;
-    git(&["pull", &args.remote, &args.branch])?;
+    git(&["pull", &args.remote, &args.branch, "--no-ff"])?;
 
     match git(&["push", &args.remote, &args.branch]) {
         Ok(_) => {
